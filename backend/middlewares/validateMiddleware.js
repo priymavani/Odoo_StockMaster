a180ff28ca864a56b49e0706832152bd3424b005
priymavani <priy.mavani.cg@gmail.com>
2025-11-22 13:03:38 +0530

    add validation middleware to handle request validation errors

---
+++ backend/middlewares/validateMiddleware.js
@@ -0,0 +1,15 @@
+const { validationResult } = require('express-validator');
+
+function handleValidation(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      error: true,
+      message: 'Validation failed',
+      details: errors.array(),
+    });
+  }
+  return next();
+}
+
+module.exports = { handleValidation };
